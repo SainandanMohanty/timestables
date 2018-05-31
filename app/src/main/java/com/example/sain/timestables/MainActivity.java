7989f906b801b7c6d1d3c60a1111b634b39addd2
Sainandan Mohanty <sainandan.mohanty.sm@gmail.com>
2018-06-01 02:55:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sain/timestables/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.sain.timestables;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        updateListView(0);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(19);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateListView(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void updateListView(int progress) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arrayList.add(Integer.toString((i + 1) * (progress + 1)));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(arrayAdapter);
+    }
+}
